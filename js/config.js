ccbe6902373f08b288e1b1f20e73ebec1d75c7da
hrezende <heberson.rezende@worldline.com>
2021-05-24 14:03:45 -0300

    add usrPoolClientId

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_8UTHPZ945', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1je0sj1k4vfsjdshn5o8lia5hu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
